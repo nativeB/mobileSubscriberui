4da20cba920a2fbdf02ef7f59fe6ec3f5024b68f
quincy <qhutchison8@gmail.com>
2022-03-26 23:14:40 +0000

    add input components

---
+++ src/components/Input.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="group">
+    <input
+      class="input-text"
+      :type="type"
+      v-model="input"
+      :required="required"
+    /><span class="highlight"></span><span class="bar"></span>
+    <label>{{ label }}</label>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Header",
+  props: {
+    type: {
+      type: String,
+      default: "text",
+    },
+    value: {
+      type: String,
+      default: "",
+    },
+    label: {
+      type: String,
+      default: "",
+    },
+    required: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  data(): { input: string } {
+    return {
+      input: "",
+    };
+  },
+  watch: {
+    value: {
+      immediate: true,
+      handler(value: string): void {
+        this.input = value;
+      },
+    }
+  },
+});
+</script>
---
+++ src/components/RadioGroup.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="input-group">
+    <label> {{ label }} </label>
+    <div class="select-group">
+      <div v-for="(item, index) in options" :key="index">
+        <input
+          type="radio"
+          :checked="item.value === input"
+          :required="required"
+          :id="`radio_${item.value}`"
+          name="radio-group-custom"
+          :value="item.value"
+        />
+        <label :for="`radio_${item.value}`">{{ item.label }}</label
+        ><br />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Header",
+  props: {
+    options: {
+      type: Array,
+      default: () => {
+        return [];
+      },
+    },
+    label: {
+      type: String,
+      default: "",
+    },
+    required: {
+      type: Boolean,
+      default: true,
+    },
+     value: {
+      type: String,
+      default: "",
+    },
+  },
+  data(): { input: string } {
+    return {
+      input: "",
+    };
+  },
+  watch: {
+    value: {
+      immediate: true,
+      handler(value: string): void {
+        this.input = value;
+      },
+    },
+
+  },
+});
+</script>
