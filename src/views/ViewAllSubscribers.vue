56ce031bed6b998fa73644edde224d788fe6e41a
quincy <qhutchison8@gmail.com>
2022-03-26 23:15:17 +0000

    add view all subscribers page

---
+++ src/views/ViewAllSubscribers.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <ListSubscribers />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import ListSubscribers from "@/components/ListSubscribers.vue"; // @ is an alias to /src
+
+export default defineComponent({
+  name: "Home",
+  components: {
+    ListSubscribers,
+  },
+});
+</script>
